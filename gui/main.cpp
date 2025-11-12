4c0ec03fd597c27f78fabfe898ed6cee68b108be
royeiror <royeiror@gmail.com>
2025-11-12 10:38:27 -0600

    Update main.cpp

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,17 +1,16 @@
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
 
-
 int main() {
-    glfwInit();
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Shrink Morph GUI", NULL, NULL);
+    if (!glfwInit()) return 1;
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Shrink Morph GUI", nullptr, nullptr);
     glfwMakeContextCurrent(window);
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 130");
+    ImGui_ImplOpenGL3_Init();
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -19,15 +18,15 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Shrink Morph Controls");
-        static char meshPath[256] = "data/beetle.obj";
-        ImGui::InputText("Mesh Path", meshPath, IM_ARRAYSIZE(meshPath));
-        if (ImGui::Button("Run Shrink Morph")) {
-            shrink_morph::run(meshPath);  // Call core logic
-        }
+        ImGui::Begin("Hello");
+        ImGui::Text("Shrink Morph GUI is running.");
         ImGui::End();
 
         ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         glfwSwapBuffers(window);
     }
